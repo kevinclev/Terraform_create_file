de90d2987889c1a7e5ffc697daafcce73a04167f
kevinclev <kevin.cleveland@fishtech.group>
2019-10-21 14:59:51 -0500

    Terraform 12 syntax uplift

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ I needed to test to some things with creating a file in Terraform so I made a Gi
 If you wanna see how files are created with Terraform and template files, then you've come to the right place. Is there really a purpose to creating files with just Terraform alone? Not really. But I started playing around with the config for another project and decided that this _could_ be a good learning resource. Idk maybe I'll drop this is a blog someday.
 
 ## Additional Info
-Terraform Version 0.11.x
+Terraform Version 0.12.x
--- make_file.tf
+++ make_file.tf
@@ -1,7 +1,7 @@
 # Create first file from template with vars defined below.
 data "template_file" "test" {
-  template = "${file("files/test.txt.tpl")}"
-  
+  template = file("files/test.txt.tpl")
+
   vars = {
     test_string = "this is a file"
   }
@@ -9,25 +9,26 @@ data "template_file" "test" {
 
 # Create second file from template file with vars and a normal file.
 data "template_file" "test2" {
-  template = "${file("files/test2.txt.tpl")}"
+  template = file("files/test2.txt.tpl")
 
   vars = {
-    test_file  = "${data.template_file.test.rendered}"
-    test_file3 = "${file("files/test3.txt")}"
+    test_file  = data.template_file.test.rendered
+    test_file3 = file("files/test3.txt")
   }
 }
 
 # Use a null resource to actually write the file.
 resource "null_resource" "write_file" {
-
   provisioner "local-exec" {
     command = <<EOT
     echo "${data.template_file.test2.rendered}" > test_file.txt
-    EOT
+    
+EOT
+
   }
 }
 
-
 output "test_file" {
-  value = "${data.template_file.test2.rendered}"
+  value = data.template_file.test2.rendered
 }
+
